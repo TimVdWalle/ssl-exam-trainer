2a237fbbba5262e89a2c0698f5e372a1581089ac
Tim Vande Walle <tim@vandewalle.mobi>
2024-03-13 20:29:07 +0100

    🌟 Update link to home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                 <div class="sm:col-span-12 lg:col-span-3">
                     <div class="mb-2">
                         <!-- Logo -->
-                        <a class="inline-block" href="index.html" aria-label="Cruip">
+                        <a class="inline-block" href="{{ route('home') }}" aria-label="Cruip">
                             <svg class="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                 <defs>
                                     <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
