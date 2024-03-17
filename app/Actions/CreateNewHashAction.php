9b9a159422f3d9d84dd10b87f0ee64640cec71e2
Tim Vande Walle <tim@vandewalle.mobi>
2024-03-17 16:37:32 +0100

    ✨ Add hash length config var

--- app/Actions/CreateNewHashAction.php
+++ app/Actions/CreateNewHashAction.php
@@ -12,11 +12,13 @@ class CreateNewHashAction
      */
     public function __invoke(): string
     {
+        /** @var int $hashLength */
+        $hashLength = config('ssl_exam_trainer.test_hash_length');
         // TODO: add code to check if hash exists in cache AND/OR in db
         // as soon as we have one collision: search further, but notify to bugsnag so that the hash length should be made bigger in the configuration (env)
 
 //        do {
-            $newHash = bin2hex(random_bytes(8)); // Generate a new hash
+            $newHash = bin2hex(random_bytes(intval($hashLength / 2))); // Generate a new hash
 //        } while (Test::where('hash_column', $newHash)->exists()); // Check if it exists
 
 
