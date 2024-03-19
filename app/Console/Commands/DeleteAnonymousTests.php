77e0b72d4034a9a54123b83ce62607a9f506858a
Tim Vande Walle <tim@vandewalle.mobi>
2024-03-19 23:11:03 +0100

    ✨ Add command to delete anonymous tests older than x days

---
+++ app/Console/Commands/DeleteAnonymousTests.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Test;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class DeleteAnonymousTests extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete-anonymous-tests';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'delete-anonymous-tests';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $this->info('delete-anonymous-tests');
+        $days = 2;
+
+        $cutoffDate = Carbon::now()->subDays($days);
+
+        // Using Query Builder to delete tests
+        /** @var int $deletedTests */
+        $deletedTests = Test::query()
+            ->whereNull('user_id') // No user associated
+            ->where('created_at', '<', $cutoffDate) // Older than x days
+            ->delete();
+
+        $this->info("Deleted $deletedTests tests that were older than $days days and had no user associated.");
+
+        return 0;
+    }
+}
