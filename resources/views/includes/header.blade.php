2094027eecf34466a868af912610d1d479316841
Tim Vande Walle <tim@vandewalle.mobi>
2024-03-26 22:17:13 +0100

    🌟 Update menu: show correct items for logged in and logged out users

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -92,17 +92,31 @@
 
                 <!-- Desktop sign in links -->
                 <ul class="flex grow justify-end flex-wrap items-center">
-                    <li>
-                        <a class="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out" href="{{ route('login') }}">Log in</a>
-                    </li>
-                    <li>
-                        <a class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" href="{{ route('register') }}">
-                            <span>Registreer</span>
-                            <svg class="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill-rule="nonzero" />
-                            </svg>
-                        </a>
-                    </li>
+
+                    @auth
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">
+                                    Uitloggen
+                                </button>
+                            </form>
+                        </li>
+                    @endauth
+
+                    @guest
+                        <li>
+                            <a class="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out" href="{{ route('login') }}">Log in</a>
+                        </li>
+                        <li>
+                            <a class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" href="{{ route('register') }}">
+                                <span>Registreer</span>
+                                <svg class="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill-rule="nonzero" />
+                                </svg>
+                            </a>
+                        </li>
+                    @endguest
                 </ul>
 
             </nav>
