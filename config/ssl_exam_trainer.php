7fef0d60fe2126fac04a5fc12fa923d0c958fdb9
Tim Vande Walle <tim@vandewalle.mobi>
2024-03-29 23:21:42 +0100

    🌟 Update config file with app wide settings

--- config/ssl_exam_trainer.php
+++ config/ssl_exam_trainer.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    'test_hash_length' => env('HASH_LENGTH', 6),
+
     'questions_per_test' => env('QUESTIONS_PER_TEST', 15),
     'min_correct_to_pass' => env('MIN_CORRECT_TO_PASS', 9),
-    'test_hash_length' => env('HASH_LENGTH', 6),
+    'max_wrong_percentage' => 40,
 ];
