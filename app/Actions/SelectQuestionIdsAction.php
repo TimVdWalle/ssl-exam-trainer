3e5c5f484fc889f4f31c4e8804e6e8778fcb3e12
Tim Vande Walle <tim@vandewalle.mobi>
2024-03-29 23:23:37 +0100

    🌟 Update SelectQuestionIdsAction with algorithm to select questions based on user questions history

--- app/Actions/SelectQuestionIdsAction.php
+++ app/Actions/SelectQuestionIdsAction.php
@@ -3,7 +3,12 @@
 namespace App\Actions;
 
 use App\Models\Question;
+use App\Models\Test;
+use App\Models\User;
+use App\Models\UserAnswer;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SelectQuestionIdsAction
 {
@@ -13,14 +18,72 @@ class SelectQuestionIdsAction
      */
     public function __invoke(): \Illuminate\Support\Collection
     {
-        // Select 15 random questions. Adjust the logic for weighting based on user history if needed.
         /** @var int $questionsPerTest */
         $questionsPerTest = config('ssl_exam_trainer.questions_per_test');
+        $questionsPerTest = intval($questionsPerTest);
+        $maxWrongQuestions = intval(floor($questionsPerTest * config('ssl_exam_trainer.max_wrong_percentage') / 100));
 
-        return Question::query()
-            ->inRandomOrder()
-            ->take(intval($questionsPerTest))
-            ->pluck('id');
+        if (Auth::check()) {
+            $userId = Auth::id();
+
+            // first, select some questions that were previously answered wrong more than they were answered correct by the user
+            $wrongAnsweredQuestionIds = UserAnswer::query()
+                ->select('question_id',
+                    DB::raw('SUM(CASE WHEN is_correct = 0 THEN 1 ELSE 0 END) as wrong_count'),
+                    DB::raw('SUM(CASE WHEN is_correct = 1 THEN 1 ELSE 0 END) as right_count'))
+                ->where('user_id', $userId)
+                ->groupBy('question_id')
+                ->havingRaw('SUM(CASE WHEN is_correct = 0 THEN 1 ELSE 0 END) > SUM(CASE WHEN is_correct = 1 THEN 1 ELSE 0 END)')
+                ->orderBy(DB::raw('RAND()'))
+                ->limit($maxWrongQuestions)
+                ->pluck('question_id')
+//                ->dd()
+            ;
+
+            // next, select some questions that were never answered before and for that we need previously answered question ids
+            $previouslyAnsweredQuestionIds = UserAnswer::query()
+                ->where('user_id', '=', $userId)
+                ->distinct()
+                ->pluck('question_id');
+
+            $neverAnsweredQuestionIds = Question::query()
+                ->whereNotIn('id', $previouslyAnsweredQuestionIds)
+                ->distinct()
+                ->orderBy(DB::raw('RAND()'))
+                ->limit($questionsPerTest - $maxWrongQuestions)
+                ->pluck('id');
+
+            $result = $wrongAnsweredQuestionIds->concat($neverAnsweredQuestionIds->toArray());
+
+            // finally, if not enough questions yet, select again from the whole pool
+            // allowing for previously answered questions (both correct or wrong) to be selected
+            $setSize = $wrongAnsweredQuestionIds->count() + $neverAnsweredQuestionIds->count();
+            if($setSize < $questionsPerTest){
+                $additionalQuestionIDs = Question::query()
+                    ->whereNotIn('id', $wrongAnsweredQuestionIds)
+                    ->whereNotIn('id', $neverAnsweredQuestionIds)
+                    ->distinct()
+                    ->orderBy(DB::raw('RAND()'))
+                    ->limit($questionsPerTest - $maxWrongQuestions)
+                    ->pluck('id');
+
+                $result = $result->concat($additionalQuestionIDs->toArray());
+            }
+
+            return $result->shuffle();
+        } else {
+            // If user is not logged in, select randomly
+            return Question::inRandomOrder()->take($questionsPerTest)->pluck('id');
+        }
+
+//        // Select 15 random questions. Adjust the logic for weighting based on user history if needed.
+//        /** @var int $questionsPerTest */
+//        $questionsPerTest = config('ssl_exam_trainer.questions_per_test');
+//
+//        return Question::query()
+//            ->inRandomOrder()
+//            ->take(intval($questionsPerTest))
+//            ->pluck('id');
     }
 
 }
