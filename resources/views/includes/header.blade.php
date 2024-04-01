eab4f4034042b8f859c8cc5d6fd72908419900d7
Tim Vande Walle <tim@vandewalle.mobi>
2024-04-01 18:01:29 +0200

    🌟 Update mobile menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -182,17 +182,31 @@
 {{--                                </li>--}}
 {{--                            </ul>--}}
 {{--                        </li>--}}
+
+                        @guest
                         <li>
-                            <a class="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center" href="{{ route('login') }}">Sign in</a>
+                            <a class="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center" href="{{ route('login') }}">Log in</a>
                         </li>
                         <li>
                             <a class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2" href="{{ route('register') }}">
-                                <span>Sign up</span>
+                                <span>Registreer</span>
                                 <svg class="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fill-rule="nonzero" />
                                 </svg>
                             </a>
                         </li>
+                        @endguest
+
+                        @auth
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                                        Uitloggen
+                                    </button>
+                                </form>
+                            </li>
+                        @endauth
                     </ul>
                 </nav>
 
