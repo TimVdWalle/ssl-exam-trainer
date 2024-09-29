0b354a1df7f6c12b851bfde00fec5a4c3232679e
Tim Vande Walle <tim@vandewalle.mobi>
2024-09-29 15:50:12 +0200

    🕸️ Fix layout for progress chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,40 +20,11 @@
                 </div>
 
 
-                <div class="max-w-sm mx-auto grid gap-6 md:grid-cols-1 lg:grid-cols-2 items-start md:max-w-2xl lg:max-w-none mb-20" data-aos-id-blocks>
-                    <div>
-                        <canvas id="myChart"></canvas>
-                    </div>
 
-                    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <hr>
 
-                    <script>
-                        const ctx = document.getElementById('myChart');
-
-                        new Chart(ctx, {
-                            type: 'line',
-                            data: {
-                                labels: {!! $metrics->progressOverTime->pluck('the_date') !!},
-                                datasets: [{
-                                    label: 'Score',
-                                    data: {!! $metrics->progressOverTime->pluck('progress') !!},
-                                    borderWidth: 1,
-                                    lineTension: 0.5,
-                                    fill: 'start'
-                                }]
-                            },
-                            options: {
-                                scales: {
-                                    y: {
-                                        beginAtZero: true
-                                    }
-                                }
-                            }
-                        });
-                    </script>
-                </div>
                 <!-- Items -->
-                <div class="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+                <div class="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none mb-20" data-aos-id-blocks>
                     <x-dashboard-item
                         title="{{ round($metrics->correctlyAnsweredQuestions / $metrics->totalQuestions * 100, 0)}}%"
                         text="Globale vooruitgang"
@@ -85,6 +56,40 @@
                     />
 
 
+                </div>
+
+                <div class="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-1 items-start md:max-w-2xl lg:max-w-none mb-20" data-aos-id-blocks>
+                    <div style="">
+                        <h1 class="text-3xl">Vooruitgang</h1>
+                        <canvas id="myChart"></canvas>
+                    </div>
+
+                    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+                    <script>
+                        const ctx = document.getElementById('myChart');
+
+                        new Chart(ctx, {
+                            type: 'line',
+                            data: {
+                                labels: {!! $metrics->progressOverTime->pluck('the_date') !!},
+                                datasets: [{
+                                    label: 'Score',
+                                    data: {!! $metrics->progressOverTime->pluck('progress') !!},
+                                    borderWidth: 1,
+                                    lineTension: 0.5,
+                                    fill: 'start'
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        });
+                    </script>
                 </div>
 
             </div>
